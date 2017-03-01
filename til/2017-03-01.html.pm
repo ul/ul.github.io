fb18dfbc1908e91a205354e0915d6ce5f5864c58
Ruslan Prokopchuk <fer.obbee@gmail.com>
2017-03-01 16:53:15 +0200

    TIL LightNote

---
+++ til/2017-03-01.html.pm
@@ -0,0 +1,15 @@
+#lang pollen
+
+◊define-meta[date]{2017-03-01}
+
+◊topic{LightNote ◊date[metas]}
+
+◊main{ Yesterday my credentials for
+◊link["https://app.lightnote.co/"]{LightNote} has been arrived, and I started to
+follow the course. But with a twist: I decided to make exercises in Extempore
+and ◊link["https://github.com/ul/lightnote"]{publish them on GitHub.} First
+chapter "Sound" is done! That's my ◊code{TIL.}
+
+Enjoy and send me patches if you are into this thing too =)
+
+}
