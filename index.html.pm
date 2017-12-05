f2149cd9fbc2e1d436233d1c8a8de49d59c820ac
Ruslan Prakapchuk <rprakapchuk@atlassian.com>
2017-12-06 10:10:53 +1100

    fix typo

--- index.html.pm
+++ index.html.pm
@@ -13,9 +13,9 @@
 
 Welcome, stranger. I'm Ruslan Prokopchuk, and it's my corner of the Web.
 
-This place is going to be a log of random thoughts, which I'm gathering and decorating these days during coffee brakes. Stay tuned and check out ◊link["/archive.html"]{Archive}!
+This place is going to be a log of random thoughts, which I'm gathering and decorating these days during coffee breaks. Stay tuned and check out ◊link["/archive.html"]{Archive}!
 
-At the daytime I do software engineering, mostly front-end. See my ◊link[cv-url]{CV} or ◊link[linked-url]{LinkedIn profile} for details. I admire functional programming, Clojure & ClojureScript, and learning OCaml for a greater good. I do a little bit of open-source, don't miss my ◊link[github-url]{GitHub} page for some fun stuff.
+At the daytime I do software engineering, mostly front-end. See my ◊link[cv-url]{CV} or ◊link[linked-url]{LinkedIn profile} for details. I admire functional programming, Clojure & ClojureScript, and I'm learning OCaml for a greater good. I do a little bit of open-source, don't miss my ◊link[github-url]{GitHub} page for some fun stuff.
 
 At the night I transform to a flute and live coding junkie. Nothing to share at the moment, but if you are interested in learning and jammming together, don't hesitate to contact me!
 
